3f26178383c72afcc3ecbe38c8433c229b1e2401
mohitsoni.25022005 <mohitsoni.25022005@gmail.com>
2025-05-10 07:48:35 +0000

    modified:   admin_panel.html 	modified:   app.js

--- app.js
+++ app.js
@@ -122,22 +122,6 @@ app.post('/admin/users', isAdmin, async (req, res) => {
   }
 });
 
-// Add a new user (might be redundant with /register, but useful for admin)
-app.post('/admin/users', isAdmin, async (req, res) => {
-  try {
-    const { name, email, password } = req.body;
-    const existingUser = await User.findOne({ email });
-    if (existingUser) return res.status(400).json({ message: 'User with that email already exists' });
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = new User({ name, email, password: hashedPassword });
-    await newUser.save();
-    res.status(201).json({ message: 'User added successfully', user: { _id: newUser._id, name: newUser.name, email: newUser.email } });
-  } catch (error) {
-    res.status(500).json({ message: 'Server error adding user', error });
-  }
-});
-
 // Update a user
 app.put('/admin/users/:id', isAdmin, async (req, res) => {
   try {
@@ -213,3 +197,4 @@ app.put('/admin/joinus/:id', isAdmin, async (req, res) => {
   } catch (error) {
     res.status(500).json({ message: 'Server error updating Join Us entry', error });
   }
+});
